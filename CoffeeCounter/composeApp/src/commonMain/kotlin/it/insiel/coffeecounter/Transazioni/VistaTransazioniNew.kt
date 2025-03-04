7c8bf84c5d435fe2c62a131780ce47426d7b816e
TM2405 <federico.delbello.ext@insiel.net>
2025-03-04 12:47:14 +0100

    Pulizia codice

--- CoffeeCounter/composeApp/src/commonMain/kotlin/it/insiel/coffeecounter/Transazioni/VistaTransazioniNew.kt
+++ CoffeeCounter/composeApp/src/commonMain/kotlin/it/insiel/coffeecounter/Transazioni/VistaTransazioniNew.kt
@@ -4,21 +4,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-//import androidx.lifecycle.viewmodel.compose.viewModel
-import it.insiel.coffeecounter.RichiesteServer.Persona
-import it.insiel.coffeecounter.RichiesteServer.Result
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 /**
  * VISTA
